6f6dffa1484f67d9bd9b382f32f00e0a4dadc564
Shubham Mishra <mshubbham7@gmail.com>
2024-09-08 23:00:04 +0530

    All initial page setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="p-2 m-2 text-red-400 text-2xl">
+            Namaste Everyone, how are you doing ?
+        </div>
+    );
 }
 
 export default App;
